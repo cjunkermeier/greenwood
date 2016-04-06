f4a6dcda77e6e839d208b8746a43f5279b50a366
junky <junkathing@users.noreply.github.com>
2016-04-06 09:49:26 -1000

    Create project.clj
    The original upload of the Greenwood library failed to include this file.

---
+++ project.clj
@@ -0,0 +1,17 @@
+(defproject greenwood "0.1.0"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [ [org.clojure/clojure "1.8.0"]
+                 [org.clojure/math.combinatorics "0.1.1"]
+                 [net.mikera/core.matrix "0.34.0"]
+                 [net.mikera/vectorz-clj "0.29.0"]
+                  [incanter/incanter "1.9.0"]
+                 [iota "1.1.2"]
+                  [criterium "0.4.3"]
+                  [me.raynes/fs "1.4.6"]
+                  [ultra-csv "0.2.0"]]
+  :main  greenwood.core
+  :target-path "target/%s"
+  :jvm-opts ^:replace ["-server" "-Xmx4G"])
