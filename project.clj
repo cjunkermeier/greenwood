dd6e51d1b0719f8e814305a8a5db93f77739884e
cjunkermeier <junkermeier@gmail.com>
2018-03-17 11:21:53 -1000

    project file change
    added the proto-repl dependencies

--- project.clj
+++ project.clj
@@ -4,17 +4,17 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [ [org.clojure/clojure "1.8.0"]
-                 [org.clojure/math.combinatorics "0.1.1"]
-                 [net.mikera/core.matrix "0.44.0"]
-                 [net.mikera/vectorz-clj "0.29.0"]
+                  [org.clojure/math.combinatorics "0.1.1"]
+                  [net.mikera/core.matrix "0.44.0"]
+                  [net.mikera/vectorz-clj "0.29.0"]
                   [ubergraph "0.4.0"]
-    [incanter/incanter "1.9.0"]
-                 [iota "1.1.3"]
+                  [incanter/incanter "1.9.0"]
+                  [iota "1.1.3"]
                   [criterium "0.4.3"]
                   [me.raynes/fs "1.4.6"]
                   [ultra-csv "0.2.0"]
-                  [random-seed "1.0.0"]]
+                  [random-seed "1.0.0"]
+                  [proto-repl "0.3.1"]]
   :main  greenwood.core
   :target-path "target/%s"
   :jvm-opts ^:replace ["-server" "-Xmx4G"])
-
